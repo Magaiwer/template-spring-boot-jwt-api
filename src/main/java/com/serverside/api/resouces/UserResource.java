da4079b4859baa16b1b64d90f12c95c3f76d6004
Magaiver Santos <magaiver@tricon.inf.br>
2020-04-28 11:36:34 -0300

    Refactoring

--- src/main/java/com/serverside/api/resouces/UserResource.java
+++ src/main/java/com/serverside/api/resouces/UserResource.java
@@ -1,11 +1,8 @@
 package com.serverside.api.resouces;
 
-import com.serverside.api.dto.UserDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Magaiver Santos
  */
 @RestController
-@CrossOrigin
 @RequestMapping(value = "/user")
 public class UserResource {
 
